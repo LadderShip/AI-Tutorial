74b62c7cc6c527f2d90cb6f52bb046e0a6b8f740
AstralBat64 <nogibator2000@gmail.com>
2024-06-29 14:35:23 +0300

    update

--- AI-Tutorial/Program.cs
+++ AI-Tutorial/Program.cs
@@ -1,16 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var Result = 0.3; //AI event
-var Timer = 0.1;  //AI atomic clock  
+var Timer = 10;  //AI atomic clock  
 while (true)
 {
     if (double.TryParse(Console.ReadLine(), out var request))
     {
-        if (Timer > 0.9)
-        {
-            Timer = 0.1;
-        }
-        Timer += 0.1;
-        for (int i = 0; i < Timer * Result; i++)
+        Timer++;
+        Timer %= 10;
+        for (int i = 0; i < Timer * Result; i++) //sync time and events
         {
             Result = Result * request;
         }
