f80b9c903c3517379c72d2a87547ead67d94f492
AstralBat64 <nogibator2000@gmail.com>
2024-06-29 14:01:33 +0300

    Add project files.

---
+++ AI-Tutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+var Result = 0.3; //AI event
+var Timer = 0.1;  //AI atomic clock  
+while (true)
+{
+    if (double.TryParse(Console.ReadLine(), out var request))
+    {
+        if (Timer > 0.9)
+        {
+            Timer = 0.1;
+        }
+        Timer += 0.1;
+        for (int i = 0; i < Timer * Result; i++)
+        {
+            Result = Result * request;
+        }
+    }
+    Console.WriteLine(Result);
+
+}
